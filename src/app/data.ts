7dc939664c2eeae27438ac79ba3d21a3623f05e0
aloima <acar.aloima@gmail.com>
2025-07-02 16:03:43 +0300

    created minimal dataset

---
+++ src/app/data.ts
@@ -0,0 +1,144 @@
+export interface Meal {
+  name: string;
+  purine: number; // milligram
+  quantity: number; // gram
+};
+
+export const meals: Meal[] = [
+  {
+    name: "1 adet haşlanmış yumurta",
+    purine: 10,
+    quantity: 100
+  },
+  {
+    name: "Dana eti (kırmızı et)",
+    purine: 135,
+    quantity: 100
+  },
+  {
+    name: "Kuzu eti",
+    purine: 150,
+    quantity: 100
+  },
+  {
+    name: "Tavuk göğsü",
+    purine: 160,
+    quantity: 100
+  },
+  {
+    name: "Hindi eti",
+    purine: 175,
+    quantity: 100
+  },
+  {
+    name: "Dana karaciğeri",
+    purine: 400,
+    quantity: 100
+  },
+  {
+    name: "Hamsi",
+    purine: 350,
+    quantity: 100
+  },
+  {
+    name: "Sardalya",
+    purine: 350,
+    quantity: 100
+  },
+  {
+    name: "Ton balığı (konserve)",
+    purine: 180,
+    quantity: 100
+  },
+  {
+    name: "Karides",
+    purine: 170,
+    quantity: 100
+  },
+  {
+    name: "Mercimek (pişmiş)",
+    purine: 60,
+    quantity: 100
+  },
+  {
+    name: "Nohut (pişmiş)",
+    purine: 65,
+    quantity: 100
+  },
+  {
+    name: "Kuru fasulye (pişmiş)",
+    purine: 60,
+    quantity: 100
+  },
+  {
+    name: "Soya fasulyesi (kuru)",
+    purine: 180,
+    quantity: 100
+  },
+  {
+    name: "Fındık",
+    purine: 10,
+    quantity: 100
+  },
+  {
+    name: "Ceviz",
+    purine: 10,
+    quantity: 100
+  },
+  {
+    name: "Badem",
+    purine: 10,
+    quantity: 100
+  },
+  {
+    name: "Ispanak",
+    purine: 55,
+    quantity: 100
+  },
+  {
+    name: "Karnabahar",
+    purine: 50,
+    quantity: 100
+  },
+  {
+    name: "Kuşkonmaz",
+    purine: 25,
+    quantity: 100
+  },
+  {
+    name: "Patates",
+    purine: 10,
+    quantity: 100
+  },
+  {
+    name: "Süt",
+    purine: 1,
+    quantity: 100
+  },
+  {
+    name: "Yoğurt",
+    purine: 5,
+    quantity: 100
+  },
+  {
+    name: "Beyaz peynir",
+    purine: 7,
+    quantity: 100
+  },
+  {
+    name: "Beyaz ekmek",
+    purine: 10,
+    quantity: 100
+  },
+  {
+    name: "Pirinç (pişmiş)",
+    purine: 8,
+    quantity: 100
+  },
+  {
+    name: "Yulaf ezmesi",
+    purine: 70,
+    quantity: 100
+  }
+];
+
