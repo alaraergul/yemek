7fd861afb22197e42c4f2e593f48e82f8f45c9fd
aloima <acar.aloima@gmail.com>
2025-07-02 20:18:05 +0300

    add id values to meals

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,4 +1,5 @@
 export interface Meal {
+  id: number;
   name: string;
   purine: number; // milligram
   quantity: number; // gram
@@ -6,136 +7,163 @@ export interface Meal {
 
 export const meals: Meal[] = [
   {
+    id: 1,
     name: "1 adet haşlanmış yumurta",
     purine: 10,
     quantity: 100
   },
   {
+    id: 2,
     name: "Dana eti (kırmızı et)",
     purine: 135,
     quantity: 100
   },
   {
+    id: 3,
     name: "Kuzu eti",
     purine: 150,
     quantity: 100
   },
   {
+    id: 4,
     name: "Tavuk göğsü",
     purine: 160,
     quantity: 100
   },
   {
+    id: 5,
     name: "Hindi eti",
     purine: 175,
     quantity: 100
   },
   {
+    id: 6,
     name: "Dana karaciğeri",
     purine: 400,
     quantity: 100
   },
   {
+    id: 7,
     name: "Hamsi",
     purine: 350,
     quantity: 100
   },
   {
+    id: 8,
     name: "Sardalya",
     purine: 350,
     quantity: 100
   },
   {
+    id: 9,
     name: "Ton balığı (konserve)",
     purine: 180,
     quantity: 100
   },
   {
+    id: 10,
     name: "Karides",
     purine: 170,
     quantity: 100
   },
   {
+    id: 11,
     name: "Mercimek (pişmiş)",
     purine: 60,
     quantity: 100
   },
   {
+    id: 12,
     name: "Nohut (pişmiş)",
     purine: 65,
     quantity: 100
   },
   {
+    id: 13,
     name: "Kuru fasulye (pişmiş)",
     purine: 60,
     quantity: 100
   },
   {
+    id: 14,
     name: "Soya fasulyesi (kuru)",
     purine: 180,
     quantity: 100
   },
   {
+    id: 15,
     name: "Fındık",
     purine: 10,
     quantity: 100
   },
   {
+    id: 16,
     name: "Ceviz",
     purine: 10,
     quantity: 100
   },
   {
+    id: 17,
     name: "Badem",
     purine: 10,
     quantity: 100
   },
   {
+    id: 18,
     name: "Ispanak",
     purine: 55,
     quantity: 100
   },
   {
+    id: 19,
     name: "Karnabahar",
     purine: 50,
     quantity: 100
   },
   {
+    id: 20,
     name: "Kuşkonmaz",
     purine: 25,
     quantity: 100
   },
   {
+    id: 21,
     name: "Patates",
     purine: 10,
     quantity: 100
   },
   {
+    id: 22,
     name: "Süt",
     purine: 1,
     quantity: 100
   },
   {
+    id: 23,
     name: "Yoğurt",
     purine: 5,
     quantity: 100
   },
   {
+    id: 24,
     name: "Beyaz peynir",
     purine: 7,
     quantity: 100
   },
   {
+    id: 25,
     name: "Beyaz ekmek",
     purine: 10,
     quantity: 100
   },
   {
+    id: 26,
     name: "Pirinç (pişmiş)",
     purine: 8,
     quantity: 100
   },
   {
+    id: 27,
     name: "Yulaf ezmesi",
     purine: 70,
     quantity: 100
