9f31c72598f0a464f90083e6bfef8e4cb65a7726
aloima <acar.aloima@gmail.com>
2025-07-04 11:20:33 +0300

    add authorization

--- backend/run.py
+++ backend/run.py
@@ -16,10 +16,10 @@ firebase = initialize_app(cred, {
 
 def check_is_valid_json(req: Request, element_count: int):
   if not req.is_json or not isinstance(req.json, dict):
-    return '{"code": 400, "message": "Body must be a JSON object."}'
+    return {"code": 400, "message": "Body must be a JSON object."}
 
   if len(req.json) != element_count:
-    return f'{{"code": 400, "message": "Body must contain exactly {element_count} members."}}'
+    return {"code": 400, "message": f"Body must contain exactly {element_count} members."}
 
   return ""
 
@@ -31,13 +31,13 @@ def post_meal(user_id):
     return error
 
   if not "id" in request.json or not isinstance(request.json["id"], int):
-    return '{"code": 400, "message": "Body must contain \"id\" key and it must be a number."}'
+    return {"code": 400, "message": "Body must contain \"id\" key and it must be a number."}
 
   if not "count" in request.json or not isinstance(request.json["count"], int):
-    return '{"code": 400, "message": "Body must contain \"count\" key and it must be a number."}'
+    return {"code": 400, "message": "Body must contain \"count\" key and it must be a number."}
 
   if not "timestamp" in request.json or not isinstance(request.json["timestamp"], int):
-    return '{"code": 400, "message": "Body must contain \"timestamp\" key and it must be a number that represents unix timestamp."}'
+    return {"code": 400, "message": "Body must contain \"timestamp\" key and it must be a number that represents unix timestamp."}
 
   ref.push(request.json)
   return ""
@@ -58,10 +58,10 @@ def delete_meal(user_id):
     return error
 
   if not "id" in request.json or not isinstance(request.json["id"], int):
-    return '{"code": 400, "message": "Body must contain \"id\" key and it must be a number."}'
+    return {"code": 400, "message": "Body must contain \"id\" key and it must be a number."}
 
   if not "timestamp" in request.json or not isinstance(request.json["timestamp"], int):
-    return '{"code": 400, "message": "Body must contain \"timestamp\" key and it must be a number that represents unix timestamp."}'
+    return {"code": 400, "message": "Body must contain \"timestamp\" key and it must be a number that represents unix timestamp."}
 
   ref = db.reference(f"/{user_id}")
   data: dict = ref.get()
@@ -74,8 +74,6 @@ def delete_meal(user_id):
   ref.set(new_data)
   return ""
 
-
-
 @app.route("/users", methods = ["GET"])
 def get_users():
   ref = db.reference("/")
@@ -86,4 +84,45 @@ def get_users():
   else:
     return list(data.keys())
 
+@app.route("/users/register", methods = ["POST"])
+def create_new_user():
+  if (error := check_is_valid_json(request, 2)) != "":
+    return error
 
+  if not "username" in request.json or not isinstance(request.json["username"], str):
+    return {"code": 400, "message": "Body must contain \"username\" key and it must be a string."}
+
+  if not "password" in request.json or not isinstance(request.json["password"], str):
+    return {"code": 400, "message": "Body must contain \"password\" key and it must be a string."}
+
+  if not "weight" in request.json or not isinstance(request.json["weight"], int):
+    return {"code": 400, "message": "Body must contain \"password\" key and it must be a string."}
+
+  ref = db.reference("/users")
+  ref = ref.push(request.json)
+
+  return {"id": ref.key(), "weight": request.json["weight"]}
+
+@app.route("/users/login", methods = ["POST"])
+def check_user_credientals():
+  if (error := check_is_valid_json(request, 2)) != "":
+    return error
+
+  if not "username" in request.json or not isinstance(request.json["username"], str):
+    return {"code": 400, "message": "Body must contain \"username\" key and it must be a string."}
+
+  if not "password" in request.json or not isinstance(request.json["password"], str):
+    return {"code": 400, "message": "Body must contain \"password\" key and it must be a string."}
+
+  ref = db.reference("/users")
+
+  if ref == None:
+    return {"code": 404, "message": "There is no user"}
+
+  users: dict = ref.get()
+
+  for key, value in users.items():
+    if value["username"] == request.json["username"] and value["password"] == request.json["password"]:
+      return {"id": key, "weight": value["weight"]}
+
+  return {"code": 403, "message": "Wrong credientals"}
---
+++ src/app/auth.service.ts
@@ -0,0 +1,69 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { API_URL } from "./environment";
+
+export interface User {
+  id: string;
+  weight: number;
+}
+
+export interface Error {
+  code: number;
+  message: string;
+}
+
+@Injectable({providedIn: "root"})
+export class AuthService {
+  public user$?: Promise<User>;
+  public error$?: string;
+
+  constructor(private http: HttpClient) {
+    if (typeof document !== "undefined" && document.cookie) {
+      const username = document.cookie.split("username=")[1].slice(-1);
+      const password = document.cookie.split("password=")[1].slice(-1);
+
+      this.http.post<User | Error>(`${API_URL}/users/login`, {username, password}).subscribe(response => {
+        if ((response as Error).code) {
+          this.error$ = (response as Error).message;
+          return;
+        }
+
+        this.user$ = Promise.resolve(response as User);
+      });
+    }
+  }
+
+  register(username: string, password: string, weight: number){
+    this.error$ = undefined;
+
+    this.http.post<User | Error>(`${API_URL}/users/register`, {username, password, weight}).subscribe(response => {
+      if ((response as Error).code) {
+        this.error$ = (response as Error).message;
+        return;
+      }
+
+      this.user$ = Promise.resolve(response as User);
+      document.cookie = `username=${username}; password=${password}; expires=Thu, 01 Jan 2099 12:00:00 UTC`;
+    });
+  }
+
+  login(username: string, password: string) {
+    this.error$ = undefined;
+
+    this.http.post<User | Error>(`${API_URL}/users/login`, {username, password}).subscribe(response => {
+      if ((response as Error).code) {
+        this.error$ = (response as Error).message;
+        return;
+      }
+
+      this.user$ = Promise.resolve(response as User);
+      document.cookie = `username=${username}; password=${password}; expires=Thu, 01 Jan 2099 12:00:00 UTC`;
+    });
+  }
+
+  logout(): void {
+    this.error$ = undefined;
+    this.user$ = undefined;
+    document.cookie = `username=; password=; expires=Thu, 01 Jan 2099 12:00:00 UTC`;
+  }
+}
